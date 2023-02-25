081478e9ce34a313c19f8fd57a8e8fd2bc1da291
Ishant Pal <mrishantsre1511ish@gmail.com>
2023-02-25 22:13:46 +0530

    contest2

---
+++ script.js
@@ -0,0 +1,8 @@
+var navLinks = document.getElementsByClassName("nav2");
+
+function showMenu(){
+    navLinks.style.right = "200px";
+}
+function hideMenu(){
+    navLinks.style.right = "-200px";
+}
